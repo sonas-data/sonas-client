87b26757eb558b8b1b3b909339333827c5f3375e
prince-1994 <yuvrajsahu007@gmail.com>
2024-12-01 16:58:58 +0100

    Initial commit

---
+++ sonas_client/client.py
@@ -0,0 +1,17 @@
+class SonasClient:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    def _login(self):
+        pass
+
+    def get_allowed_products(self):
+        return []
+
+    def get_snapshots(self):
+        return []
+
+    def get_historical(self):
+        return []
+    
